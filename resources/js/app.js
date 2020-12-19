acd97aacdf55a18760861a0e517f46e3548fb8b0
Sowren Sen <sowrensen@gmail.com>
2020-12-19 12:56:55 +0600

    configure with vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-require('./bootstrap');
+require('./bootstrap')
+
+import Vue from 'vue'
+window.Vue = Vue
+
+// Import the package's JS
+import LaravelUikit from '../../vendor/sowrensen/laravel-uikit/resources/js/laravel-uikit'
+
+const app = new Vue({
+  el: '#app',
+  mounted () {
+    // Initialize Laravel UIKit
+    const laravelUikit = new LaravelUikit()
+  }
+})
